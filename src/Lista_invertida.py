689c46e37512070b97f65d35d845ddd0f6f6ef64
MrBarreto4 <saviobarteles@poli.ufrj.br>
2024-04-11 20:31:03 -0300

    Adding NLTK processing

--- src/Lista_invertida.py
+++ src/Lista_invertida.py
@@ -1,29 +1,46 @@
 import xml.etree.ElementTree as ET
 import os
+import nltk
+import re
+import configparser
 from nltk.corpus import stopwords
 from nltk.tokenize import word_tokenize
 
-diretorio_atual = os.path.dirname(os.path.abspath(__file__))
-caminho_arquivo = os.path.join(diretorio_atual, '../data/cf74.xml')
+nltk.download('punkt')
+nltk.download('stopwords')
+stop_words = set([x.upper() for x in stopwords.words('english')])
 
-tree = ET.parse(caminho_arquivo)
-root = tree.getroot()
+diretorio_atual = os.path.dirname(os.path.abspath(__file__))
+caminho_config = os.path.join(diretorio_atual, '../cfg/GLI.CFG')
+
+configs = configparser.RawConfigParser()
+configs.read(caminho_config)
+
+output = configs.get('GLI.CFG', 'ESCREVA')
+arquivos = [os.path.join(diretorio_atual, '../data/' + x) for x in configs.get('GLI.CFG', 'LEIA').split(",")]
+
 abstracts = []
 extracts = []
 
-for artigo in root.findall('RECORD'):
-    abstract = artigo.find('ABSTRACT')
-    extract = artigo.find('EXTRACT')
-    if abstract is not None and len(abstract.text) > 0:
-        abstracts.append((artigo.find('RECORDNUM').text, abstract.text.strip()))
-    elif extract is not None and len(extract.text) > 0:
-        extracts.append((artigo.find('RECORDNUM').text, extract.text.strip()))
+for caminho_arquivo in arquivos:
+    tree = ET.parse(caminho_arquivo)
+    root = tree.getroot()
+    for artigo in root.findall('RECORD'):
+        
+        abstract = artigo.find('ABSTRACT')
+        extract = artigo.find('EXTRACT')
+        
+        if abstract is not None and len(abstract.text) > 0:
+            abstract_limpo = re.sub(r'[\'\'``,$&#=+@.%;(){}\[\]]', '', abstract.text)
+            abstract_tokens = [p.upper() for p in word_tokenize(abstract_limpo)]
+            abstract_palavras = [p for p in abstract_tokens if not p in stop_words]
+            abstracts.append((artigo.find('RECORDNUM').text, abstract_palavras))
+        
+        elif extract is not None and len(extract.text) > 0:
+            extract_limpo = re.sub(r'[\'\'``,$&#=+@.%;(){}\[\]]', '', extract.text)
+            extract_tokens = [p.upper() for p in word_tokenize(extract_limpo)]
+            extract_palavras = [p for p in extract_tokens if not p in stop_words]
+            extracts.append((artigo.find('RECORDNUM').text, extract_palavras))
 
-for id, abstract in abstracts:
-    print(f"Abstract {id}: {abstract}")
-    print()
-
-for id, extract in extracts:
-    print(f"Extract {id}: {extract}")
-    print()
-
+for desc in extracts:
+    print(desc)
