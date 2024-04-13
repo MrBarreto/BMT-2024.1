1d07692924726ef082718467031b6988b46a60fa
MrBarreto <saviobarteles@poli.ufrj.br>
2024-04-13 14:25:42 -0300

    Adicionando protoIndexador

--- src/Indexador.py
+++ src/Indexador.py
@@ -5,8 +5,8 @@ import re
 import configparser
 import csv
 import logging
-from nltk.corpus import stopwords
-from nltk.tokenize import word_tokenize
+import numpy as np
+import math
 
 diretorio_atual = os.path.dirname(os.path.abspath(__file__))
 caminho_config = os.path.join(diretorio_atual, '../cfg/INDEX.CFG')
@@ -34,3 +34,46 @@ with open(caminho_lista, ) as input_csv:
 input_csv.close()
 
 id_artigos = sorted(set(id_artigos))
+
+dic_palavras = {}
+dic_artigos ={}
+dic_palavras_inv = {}
+dic_artigos_inv ={}
+
+col =  len(id_artigos)
+lin = len(dict.keys())
+palavras = list(dict.keys())
+
+for i in range(col):
+    dic_artigos[id_artigos[i]] = i
+    dic_artigos_inv[i] = id_artigos[i]
+
+for i in range(lin):
+    dic_palavras[palavras[i]] = i
+    dic_palavras_inv[i] = palavras[i]
+
+vetorial  = np.zeros((lin, col), dtype= float)
+
+for word, lista_artigos in dict.items():
+    for artigo in lista_artigos:
+        vetorial[dic_palavras[word]][dic_artigos[artigo]] = 1
+"""
+print(vetorial)
+for linha in range(len(vetorial)):
+    somatorio = 0
+    for coluna in range(len(vetorial[0])):
+        if vetorial[linha][coluna] == 1:
+            elemento = dic_artigos_inv[coluna]
+            if elemento in dict[dic_palavras_inv[linha]]:
+                somatorio += 1
+    if somatorio != len(set(dict[dic_palavras_inv[linha]])):
+        print("fodeu")
+"""
+for linha in range(len(vetorial)):
+    idf = 0
+    for coluna in range(len(vetorial[0])):
+        idf += vetorial[linha][coluna]
+    idf = math.log10(len(vetorial[0])/idf)
+    for coluna in range(len(vetorial[0])):
+        vetorial[linha][coluna] = vetorial[linha][coluna]*idf
+print(vetorial)
